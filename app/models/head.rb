c9e5344574fb17a87028e5ab2a94c86058f5a71d
John Madden <jck13mad@gmail.com>
2021-01-09 11:36:34 -0600

    Added user.rb code to head.rb by accident

--- app/models/head.rb
+++
@@ -1,9 +0,0 @@
-class User < ActiveRecord::Base
-
-    has_secure_password
-    validates :username, :uniqueness => {:case_sesnitive => false}
-    validates_presence_of :username, :email, :password
-
-    has_many :heads
-
-end
