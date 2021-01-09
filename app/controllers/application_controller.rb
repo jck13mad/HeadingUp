a1336a6688148ac41a6d74a3467544b134a81c13
John Madden <jck13mad@gmail.com>
2021-01-09 10:31:36 -0600

    created logged_in? and current_user methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,13 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  def logged_in
+    !!session[:user_id]
+  end
+
+  def current_user
+    User.find(session[:user_id])
+  end
+
+
 end
