8e55edda40989a6619922af06bbc127cd8b163a2
John Madden <jck13mad@gmail.com>
2021-01-09 13:14:08 -0600

    Added post for 'signup' and get for 'login'

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -9,6 +9,26 @@ class UserController < ApplicationController
         end
     end 
 
+    post '/signup' do
+        user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
+        
+        if user.save 
+            (session[:user_id] = user.id)
+            redirect "/heads"
+        else
+            redirect '/signup'
+        end
+    end
+
+    get '/login' do
+        user = User.find(:username => params[:username])
+        
+        if !logged_in? 
+            erb :'/users/login'
+        else
+            redirect '/heads'
+        end
+    end
 
 
 
