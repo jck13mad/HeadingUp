30f077c44386b3a319809c3ac479c46d8731dd02
John Madden <jck13mad@gmail.com>
2021-01-09 10:16:17 -0600

    initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,16 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    register Sinatra::Flash
+  end
+
+  get "/" do
+    erb :welcome
+  end
+
+end
