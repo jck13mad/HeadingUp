6edbaee1ceec79d75dddb315dfdcfb46344c5070
John Madden <jck13mad@gmail.com>
2021-01-09 13:43:51 -0600

    Added get for heads index, added erb for heads index

---
+++ app/controllers/head_controller.rb
@@ -0,0 +1,14 @@
+class HeadController < ApplicationController
+
+    get '/heads' do
+        if logged_in? 
+            @heads = Head.all
+            erb :'/heads/index'
+        else
+            redirect "/login"
+        end
+    end
+
+
+
+end
